3059d257833cc887add30088748222fa57a15d1e
rhodinemma <rhodinemma10@gmail.com>
2023-07-08 14:09:54 +0300

    feat: add mongoose for mongodb and generate todo resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://localhost/rhodin'), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
