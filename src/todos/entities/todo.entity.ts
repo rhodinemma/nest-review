9f9b28ee97452706763cce540b8d78e5bba812eb
rhodinemma <rhodinemma10@gmail.com>
2023-07-08 14:19:17 +0300

    feat: create mongodb schema

--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,1 +1,24 @@
-export class Todo {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type TodoDocument = Todo & Document;
+@Schema({
+  toJSON: {
+    virtuals: true,
+    getters: true,
+  },
+})
+export class Todo {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  completed: boolean;
+}
+
+const TodoSchema = SchemaFactory.createForClass(Todo);
+TodoSchema.virtual('id').get(function (this: TodoDocument) {
+  return this._id.toHexString();
+});
+
+export { TodoSchema };
